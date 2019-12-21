b93405ba7a2df71fbe8ad71caaf40f8bf714f9c9
matthexx <matthew_imadiyi@yahoo.com>
2019-12-21 15:14:34 -0800

    commit for backup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,298 @@
+
+<!DOCTYPE html>
+<html>
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+	<title>Codev Hub </title>
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<meta name="keywords" content="Codevelopers, Codev, Hub"/>
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+    </script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- //Meta tag Keywords -->
+	<!-- Favicon file -->
+	<link rel="icon" href="images/favicon.png">
+
+	<!-- Custom-Files -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+    {{-- <link href="css/svgs/solids" rel="stylesheet"> --}}
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //Custom-Files -->
+
+	<!-- Web-Fonts -->
+	<link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=devanagari,latin-ext"
+	 rel="stylesheet">
+	<!-- //Web-Fonts -->
+
+
+</head>
+
+<body>
+	<!-- main banner -->
+	<div class="main-top" id="home">
+		<!-- header -->
+		<header>
+			<div class="container-fluid" id="design">
+				<div class="header d-lg-flex justify-content-between align-items-center py-3 px-sm-3">
+					<!-- logo -->
+					<div id="logo1">
+						<a href="index.php"><img src="images/devlogo2.png" alt="" id="logo1" class="img-fluid"></a>
+					</div>
+					<!-- //logo -->
+					<!-- nav -->
+					<div class="nav_w3ls">
+						<nav>
+							<label for="drop" class="toggle">menu</label>
+							<input type="checkbox" id="drop" />
+							<ul class="menu">
+								<li><a href="about">About Us</a></li>
+								<li><a href="faq">FAQs</a></li>	
+								<li><a href="contact-us">Contact Us</a></li>
+							</ul>
+						</nav>
+					</div>
+					<!-- //nav -->
+					
+				</div>
+			</div>
+		</header>
+		<!-- //header -->
+
+		<!-- banner -->
+		<div class="banner_w3lspvt position-relative">
+			<div class="container">
+				<div class="d-md-flex">
+					<div class="w3ls_banner_txt">
+						<h3 class="w3ls_pvt-title">Developer <br />Career</h3>
+						<p class="text-sty-banner">A non-profit Tech organization to support the rising Tech Ecosystem in Africa. 
+						<hr/> A new day, another opportunity to be world class 🔥🚀 </p>
+						<a href="https://devcareer.io/join" class="btn button-style mt-md-5 mt-4">Create mini CV</a>
+						
+					</div>
+					<div class="banner-img">
+						<img src="images/a.png" alt="" class="img-fluid">
+					</div>
+				</div>
+			</div>
+			<!-- animations effects -->
+			<div class="icon-effects-w3l">
+				<img src="images/shape1.png" alt="" class="img-fluid shape-wthree shape-w3-one">
+				<img src="images/shape2.png" alt="" class="img-fluid shape-wthree shape-w3-two">
+				<img src="images/shape3.png" alt="" class="img-fluid shape-wthree shape-w3-three">
+				<img src="images/shape5.png" alt="" class="img-fluid shape-wthree shape-w3-four">
+				<img src="images/shape4.png" alt="" class="img-fluid shape-wthree shape-w3-five">
+			</div>
+			<!-- //animations effects -->
+		</div>
+		<!-- //banner -->
+	</div>
+	<!-- //main banner -->
+
+	<!-- what we do section -->
+	<div class="what bg-li py-5" id="what">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="row about-bottom-w3l text-center mt-4">
+				<div class="col-lg-4 about-grid">
+					<div class="about-grid-main">
+						<img src="images/think.png" alt="" class="img-fluid">
+						<h4 class="my-4">Have The Passion?</h4>
+						<p>Are you trying to get a foothold in tech? Are you having problems selecting an area of programming you wish to major in?
+							Or you need tools to aid your learning? 
+						</p>
+						<a href="#" class="button-w3ls btn mt-sm-5 mt-4 disabled">Would you</a>
+					</div>
+				</div>
+				<div class="col-lg-4 about-grid my-lg-0 my-5">
+					<div class="about-grid-main">
+						<img src="images/time.png" alt="" class="img-fluid">
+						<h4 class="my-4">Have The Time?</h4>
+						<p>Shoot us a one tweet pitch and tell us why you'd love to join our program, be rest assured, this is 
+							completely FREE. Are you available to kickstart your tech career?
+						</p>
+						<a href="#" class="button-w3ls btn mt-sm-5 mt-4 disabled">Love to</a>
+					</div>
+				</div>
+				<div class="col-lg-4 about-grid">
+					<div class="about-grid-main">
+						<img src="images/tech.png" alt="" class="img-fluid">
+						<h4 class="my-4">join Us</h4>
+						<p>We are a non-profit tech organization that's dedicated to supporting the rising tech ecosystem
+							in Africa. If you have the zeal to learn, try applying NOW!!!
+						</p>
+						<a href="https://docs.google.com/forms/d/1OP0uDPnnfZ7zQ4waOgIx80fPo8RPtJzN1fJCiIVmHOo" class="button-w3ls btn mt-sm-5 mt-4">APPLY ?</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //what we do section -->
+
+	<!-- middle -->
+	<section class="midd-w3 py-5" id="faq">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="row">
+				<div class="col-lg-6 about-right-faq">
+					<h6>Africa TechEcosystem++</h6>
+					<h3 class="text-da">Why we exist </h3>
+					<p class="mt-4">We are not planning to do much, we just want to: </p>
+					<ul class="w3l-right-book mt-4">
+						<li>Create world class Software Developers</li>
+						<li>Solve problems in Africa with Tech</li>
+						<li>Create employment and breed innovative ideas</li>
+						<li>build more positive tech savvy youths</li>
+						<li>Support females into tech in Africa</li>
+					</ul>
+					<a href="https://paystack.com/pay/laptops4developers" class="btn button-style button-style-2 mt-sm-5 mt-4">DONATE(Paystack)</a>
+					<a href="https://www.gofundme.com/z7q9rs-laptops4developerss" class="btn button-style button-style-2 mt-sm-5 mt-4">DONATE(Gofundme)</a>
+				</div>
+				<div class="col-lg-6 left-wthree-img text-right">
+					<img src="images/f.png" alt="" class="img-fluid mt-5" />
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //middle -->
+
+	<!-- services -->
+	<section class="banner-bottom-w3layouts bg-li py-5" id="services">
+		<div class="container py-xl-5 py-lg-3">
+			<h3 class="tittle text-center font-weight-bold">Our Method</h3>
+			<p class="sub-tittle text-center mt-3 mb-sm-5 mb-4">Your journey of a thousand miles, starts with a one tweet pitch..</p>
+			<div class="row pt-lg-4">
+				<div class="col-lg-4 about-in text-center">
+					<div class="card">
+						<div class="card-body">
+							<div class="bg-clr-w3l">
+								<span class="fa fa-line-chart"></span>
+							</div>
+							<h5 class="card-title mt-4 mb-3">Join Program</h5>
+							<p class="card-text">Pass our four-level stage assesment and join DevCareer. Assessment will be based on several criteria, from your pitch tweet, to your online assessment and physical interview.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 about-in text-center my-lg-0 my-3">
+					<div class="card active">
+						<div class="card-body">
+							<div class="bg-clr-w3l">
+							<span class="fa fa-code"></span>
+							</div>
+							<h5 class="card-title mt-4 mb-3">Resources && Mentors</h5>
+							<p class="card-text">Get a laptop, get a course, get paired with a mentor in your field to suport and progress your skill</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 about-in text-center">
+					<div class="card">
+						<div class="card-body">
+							<div class="bg-clr-w3l">
+							<span class="fa fa-usd"></span>
+							</div>
+							<h5 class="card-title mt-4 mb-3">Get Job placement</h5>
+							<p class="card-text">During and after your mentorship program, We will work together 
+								with your mentor to source for suitable jobs for you. You also have a choice to 
+								travel to another country for a paid internship</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //services -->
+
+	<!-- stats -->
+	<section class="bottom-count py-5" id="stats">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="row">
+				<div class="col-lg-5 left-img-w3ls">
+					<img src="images/e.png" alt="" class="img-fluid" />
+				</div>
+				<div class="col-lg-7 right-img-w3ls pl-lg-4 mt-lg-2 mt-4">
+					<div class="bott-w3ls mr-xl-5">
+						<h3 class="title-w3 text-bl mb-3 font-weight-bold">All about us in one DIV </h3>
+						<p class="title-sub-2 mb-3">A not for Profit tech organization to support more Africans into the tech World</p>
+						<p>We've not done alot, hopefully we will get to increase these numbers soon. </p>
+					</div>
+					<div class="row mt-5">
+						<div class="col-sm-4 count-w3ls">
+							<h4>0</h4>
+							<p>Developers trained</p>
+						</div>
+						<div class="col-sm-4 count-w3ls mt-sm-0 mt-3">
+							<h4>8</h4>
+							<p>Partnership</p>
+						</div>
+						<div class="col-sm-4 count-w3ls mt-sm-0 mt-3">
+							<h4>0</h4>
+							<p>Award Winning</p>
+						</div>
+					</div>
+					<div class="row mt-sm-5 mt-4">
+						<div class="col-sm-4 count-w3ls">
+							<h4>48+</h4>
+							<p>Donations</p>
+						</div>
+						<div class="col-sm-4 count-w3ls mt-sm-0 mt-3">
+							<h4>0</h4>
+							<p>Patrons</p>
+						</div>
+						<div class="col-sm-4 count-w3ls mt-sm-0 mt-3">
+							<h4>22+</h4>
+							<p>Available mentors</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //stats -->
+
+
+	<!-- footer -->
+	<footer class="bg-li py-5">
+		<div class="container py-xl-5 py-lg-3">
+			<!-- subscribe -->
+			<div class="subscribe mx-auto">
+				<div class="icon-effect-w3">
+					<span class="fa fa-envelope"></span>
+				</div>
+				<h2 class="tittle text-center font-weight-bold">Stay Updated!</h2>
+				<p class="sub-tittle text-center mt-3 mb-sm-5 mb-4">HEY! DevCareer, seems like I'll love to get your newsteller to know when your next program will be holding</p>
+				<form action="#" method="post" class="subscribe-wthree pt-2">
+					<div class="d-flex subscribe-wthree-field">
+						<input class="form-control" type="email" placeholder="So I'll put my email here right?" name="email" required="">
+						<button class="btn form-control w-50" type="submit">I'm in</button>
+					</div>
+				</form>
+			</div>
+			<!-- //subscribe -->
+		</div>
+	</footer>
+	<!-- footer -- end-->
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5cccfb46d07d7e0c6391dcb3/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</body>
+
+</html>
